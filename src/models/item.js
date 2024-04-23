a0602f92b2bfb87a5e346a3d6409eafde31b1f47
Mwallace2002 <Maxwallace2002@gmail.com>
2024-04-22 21:56:24 -0400

    reset mongoDB

--- src/models/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const itemSchema = new mongoose.Schema({
-  id: String,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model("Prueba.usuarios", itemSchema);
